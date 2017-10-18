76a52ea4c9cda46dd30c9d9fd625aa805730fbb8
Aidan Rypens <Aidan.rypens@2commit.be>
2017-10-18 16:33:13 +0200

    Zaalplan

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public zaalplan;
+  public rijen;
+
+  constructor() {
+    this.bouwZaalpanMlc();
+  }
+
+  bouwZaalpanMlc() {
+    var rijen = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R"]
+
+    let zaalplan = new Array();
+    for (let i = 0; i < 18; i++) {
+      let stoel;
+      for (let j = 1; j <= 36; j++) {
+        stoel = {
+          "id": rijen[i] + j,
+          "tekst": j,
+          "isBeschikbaar": true,
+          "isUitgeschakeld": false
+        }
+        stoel.rij = rijen[i];
+        zaalplan.push(stoel);
+      }
+    }
+
+    this.zaalplan = zaalplan;
+    this.rijen = rijen;
+    //console.log(this.zaalplan);
+  }
 }
